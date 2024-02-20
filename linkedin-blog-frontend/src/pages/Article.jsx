00868b9c0241f280366ab5845bab71571cb37999
Maverick <sc20ml@leeds.ac.uk>
2024-02-20 14:25:22 +0000

    Axios installed. frontend is retrieving data from server db

--- linkedin-blog-frontend/src/pages/Article.jsx
+++ linkedin-blog-frontend/src/pages/Article.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import articles from './article-content';
 import NotFound from './NotFound';
+import axios from 'axios';
 
 const Article = () => {
-	const { articleId } = useParams();
+
+	const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []}); 
+	const { articleId } = useParams(); // Gets the article id from the url parameter
+
+	// Load data from server and initialise comments and upvotes
+	useEffect( () => {
+		const loadArticleInfo = async() => {
+			const response = await axios.get(`/api/articles/${articleId}`);
+			const articleInfoDB = response.data;
+			setArticleInfo(articleInfoDB);
+		} 
+		
+		loadArticleInfo();
+	}, []);
+
 	const article = articles.find(article => article.name === articleId);
 
 	if(!article) {
@@ -14,6 +29,7 @@ const Article = () => {
 	return (
 		<> 
 			<h1> {article.title} </h1>
+			<p> This article has {articleInfo.upvotes} upvotes </p>
 			{article.content.map( (paragraph,i) => (
 				<p key={i}> {paragraph} </p>
 			)
