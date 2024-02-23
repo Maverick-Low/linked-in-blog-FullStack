6038ef71fc6114a402f36a38fbdbaa397e934bbc
Maverick <sc20ml@leeds.ac.uk>
2024-02-23 14:14:47 +0000

    Navbar has logout/login button - working

--- linkedin-blog-frontend/src/NavBar.jsx
+++ linkedin-blog-frontend/src/NavBar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import useUser from './hooks/useUser';
+import { signOut, getAuth } from 'firebase/auth';
 
 function NavBar() {
+    const {user} = useUser();
+    const navigate = useNavigate();
   return (
     <nav>
         <ul>
             <li><Link to = "/"> Home </Link></li>
             <li><Link to="/about"> About </Link></li>
             <li><Link to="/Articles"> Articles </Link></li>
+            {user 
+                ? <button onClick = {() => {signOut(getAuth())}}> Log out </button> 
+                :<button onClick={() => {navigate('/login')}}> Log in</button>
+            }
         </ul>
     </nav>
   )
